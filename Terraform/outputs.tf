710e9d8772004f62d2e3c3ead13fdf0f2423527c
Cautchy <104728544Cautchybailly@users.noreply.github.com>
2026-04-13 17:42:15 -0400

    syntax errors in output file fixed to match resource names

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
   description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.class7_gut_check_bucket.id
+  value       = aws_s3_bucket.class7_gutcheck_bucket.id
 }
 
 output "bucket_arn" {
   description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.class7_gut_check_bucket.arn
+  value       = aws_s3_bucket.class7_gutcheck_bucket.arn
 }
